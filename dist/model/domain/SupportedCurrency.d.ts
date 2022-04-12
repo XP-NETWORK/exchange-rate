2b0eb7df5dbe938d7111dd005593f5dff2e8d7dd
imsk17 <imsumitkumar17@gmail.com>
2022-04-12 13:25:14 +0000

    Build: (ffd273e) xdai: update currency. use GNO instead of STAKE

--- dist/model/domain/SupportedCurrency.d.ts
+++ dist/model/domain/SupportedCurrency.d.ts
@@ -11,7 +11,7 @@ export declare enum SupportedCurrency {
     TRX = "TRX",
     CELO = "CELO",
     ONT = "ONT",
-    STAKE = "STAKE",
+    GNO = "GNO",
     ALGO = "ALGO",
     FUSE = "FUSE",
     OPL = "OPL",
