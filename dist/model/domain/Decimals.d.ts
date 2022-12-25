023cc027bbece225f87d98fc2b5111838b52c19d
imsk17 <imsumitkumar17@gmail.com>
2022-12-25 18:28:33 +0000

    Build: (2555be7) Decimals: Import Decimals Data

--- dist/model/domain/Decimals.d.ts
+++ dist/model/domain/Decimals.d.ts
@@ -1,4 +1,4 @@
-export declare const Decimals: {
+export declare const DecimalsByCurrency: {
     readonly EGLD: 1000000000000000000;
     readonly HT: 1000000000000000000;
     readonly BNB: 1000000000000000000;
@@ -6,13 +6,14 @@ export declare const Decimals: {
     readonly AVAX: 1000000000000000000;
     readonly MATIC: 1000000000000000000;
     readonly FTM: 1000000000000000000;
-    readonly TRX: 1000000000000000000;
+    readonly TRX: 1000000;
     readonly CELO: 1000000000000000000;
     readonly ONE: 1000000000000000000;
     readonly ONT: 1000000000000000000;
     readonly XDAI: 1000000000000000000;
     readonly ALGO: 1000000;
     readonly FUSE: 1000000000000000000;
+    readonly OPL: 1000000000000000000;
     readonly XTZ: 1000000;
     readonly VLX: 1000000000000000000;
     readonly IOTX: 1000000000000000000;
@@ -29,6 +30,43 @@ export declare const Decimals: {
     readonly NEAR: 1e+24;
     readonly GLMR: 1000000000000000000;
     readonly ABEY: 1000000000000000000;
+    readonly APTOS: 1000000;
     readonly CMP: 1000000000000000000;
     readonly OKT: 1000000000000000000;
+};
+export declare const DecimalsByNonce: {
+    readonly 2: 1000000000000000000;
+    readonly 3: 1000000000000000000;
+    readonly 4: 1000000000000000000;
+    readonly 5: 1000000000000000000;
+    readonly 6: 1000000000000000000;
+    readonly 7: 1000000000000000000;
+    readonly 8: 1000000000000000000;
+    readonly 9: 1000000;
+    readonly 11: 1000000000000000000;
+    readonly 12: 1000000000000000000;
+    readonly 13: 1000000000000000000;
+    readonly 14: 1000000000000000000;
+    readonly 15: 1000000;
+    readonly 16: 1000000000000000000;
+    readonly 17: 1000000000000000000;
+    readonly 18: 1000000;
+    readonly 19: 1000000000000000000;
+    readonly 20: 1000000000000000000;
+    readonly 21: 1000000000000000000;
+    readonly 22: 1000000000000000000;
+    readonly 23: 1000000000000000000;
+    readonly 24: 1000000;
+    readonly 25: 1000000000000000000;
+    readonly 26: 1000000000;
+    readonly 27: 1000000000;
+    readonly 28: 100000000;
+    readonly 29: 1000000;
+    readonly 30: 1000000000000000000;
+    readonly 31: 1e+24;
+    readonly 32: 1000000000000000000;
+    readonly 33: 1000000000000000000;
+    readonly 34: 1000000;
+    readonly 35: 1000000000000000000;
+    readonly 36: 1000000000000000000;
 };
