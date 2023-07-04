91b04df908dbc3a08227d0beae765f1bce7172b1
imsk17 <imsumitkumar17@gmail.com>
2023-07-04 12:48:01 +0000

    Build: (c4cc0fe) fix exchange rate

--- dist/model/network/ExchangeRateDtoMapper.js
+++ dist/model/network/ExchangeRateDtoMapper.js
@@ -2,13 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.exchangeRateDtoMapper = void 0;
 const domain_1 = require("../domain");
+const rev = Object.fromEntries(Object.entries(domain_1.SupportedCurrency).map(([a, b]) => {
+    return [b, a];
+}));
 function exchangeRateDtoMapper() {
     return {
-        toDomain: (model) => new Map(Object.entries(model).map(([ent, v]) => [
-            domain_1.SupportedCurrency[ent],
-            v,
-        ])),
+        toDomain: (model) => new Map(Object.entries(model).map(([ent, v]) => {
+            return [domain_1.SupportedCurrency[rev[ent]], v];
+        })),
     };
 }
 exports.exchangeRateDtoMapper = exchangeRateDtoMapper;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRXhjaGFuZ2VSYXRlRHRvTWFwcGVyLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL21vZGVsL25ldHdvcmsvRXhjaGFuZ2VSYXRlRHRvTWFwcGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLHNDQUE4QztBQVU5QyxTQUFnQixxQkFBcUI7SUFDakMsT0FBTztRQUNILFFBQVEsRUFBRSxDQUFDLEtBQXNCLEVBQUUsRUFBRSxDQUNqQyxJQUFJLEdBQUcsQ0FDSCxNQUFNLENBQUMsT0FBTyxDQUFDLEtBQUssQ0FBQyxDQUFDLEdBQUcsQ0FBQyxDQUFDLENBQUMsR0FBRyxFQUFFLENBQUMsQ0FBQyxFQUFFLEVBQUUsQ0FBQztZQUNwQywwQkFBaUIsQ0FBQyxHQUFZLENBQUM7WUFDL0IsQ0FBQztTQUNKLENBQUMsQ0FDTDtLQUNSLENBQUM7QUFDTixDQUFDO0FBVkQsc0RBVUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRXhjaGFuZ2VSYXRlRHRvTWFwcGVyLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL21vZGVsL25ldHdvcmsvRXhjaGFuZ2VSYXRlRHRvTWFwcGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLHNDQUE4QztBQVU5QyxNQUFNLEdBQUcsR0FBRyxNQUFNLENBQUMsV0FBVyxDQUMxQixNQUFNLENBQUMsT0FBTyxDQUFDLDBCQUFpQixDQUFDLENBQUMsR0FBRyxDQUFDLENBQUMsQ0FBQyxDQUFDLEVBQUUsQ0FBQyxDQUFDLEVBQUUsRUFBRTtJQUM3QyxPQUFPLENBQUMsQ0FBQyxFQUFFLENBQUMsQ0FBVSxDQUFDO0FBQzNCLENBQUMsQ0FBQyxDQUNMLENBQUM7QUFFRixTQUFnQixxQkFBcUI7SUFDakMsT0FBTztRQUNILFFBQVEsRUFBRSxDQUFDLEtBQXNCLEVBQUUsRUFBRSxDQUNqQyxJQUFJLEdBQUcsQ0FDSCxNQUFNLENBQUMsT0FBTyxDQUFDLEtBQUssQ0FBQyxDQUFDLEdBQUcsQ0FBQyxDQUFDLENBQUMsR0FBRyxFQUFFLENBQUMsQ0FBQyxFQUFFLEVBQUU7WUFDbkMsT0FBTyxDQUFDLDBCQUFpQixDQUFDLEdBQUcsQ0FBQyxHQUFHLENBQVUsQ0FBQyxFQUFFLENBQUMsQ0FBQyxDQUFDO1FBQ3JELENBQUMsQ0FBQyxDQUNMO0tBQ1IsQ0FBQztBQUNOLENBQUM7QUFURCxzREFTQyJ9
